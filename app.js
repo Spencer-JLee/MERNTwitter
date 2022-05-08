1a9a55842d7bf778de77040ad4273b2033cba831
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-05-08 16:30:03 -0400

    heroku ready

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -11,6 +12,12 @@ mongoose
   .catch(err => console.log(err));
 
 // app.get("/", (req, res) => res.send("Hello World"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
