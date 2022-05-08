17c360f327d18db976945ea11b206e1535cc38e5
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-05-07 23:10:43 -0400

    finished user auth

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
